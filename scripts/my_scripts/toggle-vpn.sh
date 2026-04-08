2e61432a36afe4d001079a59d8c9343b92bdd65e
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2026-04-08 17:37:23 +0200

    chore: add toggle script for VPN connection management

---
+++ scripts/my_scripts/toggle-vpn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VPN_NAME="Freebox-Leloup"
+
+if nmcli connection show --active | grep -q "$VPN_NAME"; then
+    nmcli connection down "$VPN_NAME"
+else
+    nmcli connection up "$VPN_NAME"
+fi
